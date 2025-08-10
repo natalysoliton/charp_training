01c328b3f8ee1d6c6bc233b50a54fbc27b240b69
Ermakova Natalia <natalysolitonsf@yandex.com>
2025-08-10 15:43:43 +0300

    Добавлены файлы (задание 3 - https://software-testing.ru/lms/mod/assign/view.php?id=510690)

--- csharptests/GroupCreationTests.cs
+++ csharptests/GroupCreationTests.cs
@@ -44,35 +44,72 @@ namespace WebAddressbookTests
         [Test]
         public void GroupCreationTest()
         {
-            // Open home page
-            driver.Navigate().GoToUrl(baseURL + "/addressbook/");
-            // Login
-            driver.FindElement(By.Name("user")).Clear();
-            driver.FindElement(By.Name("user")).SendKeys("admin");
-            driver.FindElement(By.Name("pass")).Clear();
-            driver.FindElement(By.Name("pass")).SendKeys("secret");
-            driver.FindElement(By.XPath("//input[@value='Login']")).Click();
-            // Go to groups page
-            driver.FindElement(By.LinkText("groups")).Click();
-            // Init new group creation
-            driver.FindElement(By.Name("new")).Click();
-            // Fill group form
-            driver.FindElement(By.Name("group_name")).Clear();                    
-            driver.FindElement(By.Name("group_name")).SendKeys("q");
-           // driver.FindElement(By.Name("group_header")).Click();
-            driver.FindElement(By.Name("group_header")).Clear();
-            driver.FindElement(By.Name("group_header")).SendKeys("qq");
-          // driver.FindElement(By.Name("group_footer")).Click();
-            driver.FindElement(By.Name("group_footer")).Clear();
-            driver.FindElement(By.Name("group_footer")).SendKeys("qq");
-            // Submit group creation
-            driver.FindElement(By.Name("submit")).Click();
-            // Return to groups page
-            driver.FindElement(By.LinkText("group page")).Click();
-          //  driver.FindElement(By.LinkText("groups")).Click();
-          //  driver.FindElement(By.LinkText("Logout")).Click();
-            
+            OpenHomePage();
+            Login(new AccountData("admin", "secret"));
+            GoToGroupsPage();
+            InitGroupCreation();
+            GroupData group = new GroupData("aaa");
+            group.Header = "ddd";
+            group.Footer = "ccc";
+            FillGroupForm(group);
+            SubmitGroupCreation();
+            ReturnToGroupsPage();
+
         }
+
+        private void ReturnToGroupsPage()
+        {
+           
+            driver.FindElement(By.LinkText("group page")).Click();
+        }
+
+        private void SubmitGroupCreation()
+        {
+            
+            driver.FindElement(By.Name("submit")).Click();
+        }
+
+        //   private void FillGroupForm(string name, string header, string footer)
+        private void FillGroupForm(GroupData group)
+        {
+          
+            driver.FindElement(By.Name("group_name")).Clear();
+            driver.FindElement(By.Name("group_name")).SendKeys(group.Name);
+            driver.FindElement(By.Name("group_header")).Clear();
+            driver.FindElement(By.Name("group_header")).SendKeys(group.Header);
+            driver.FindElement(By.Name("group_footer")).Clear();
+            driver.FindElement(By.Name("group_footer")).SendKeys(group.Footer);
+        }
+
+        private void InitGroupCreation()
+        {
+           
+            driver.FindElement(By.Name("new")).Click();
+        }
+
+        private void GoToGroupsPage()
+        {
+           
+            driver.FindElement(By.LinkText("groups")).Click();
+        }
+
+        private void Login(AccountData account)
+        {
+         
+            driver.FindElement(By.Name("user")).Clear();
+            driver.FindElement(By.Name("user")).SendKeys(account.Username);
+            driver.FindElement(By.Name("pass")).Clear();
+            driver.FindElement(By.Name("pass")).SendKeys(account.Password);
+            driver.FindElement(By.XPath("//input[@value='Login']")).Click();
+        }
+
+   
+
+        private void OpenHomePage()
+        {
+            driver.Navigate().GoToUrl(baseURL + "/addressbook/");
+        }
+
         private bool IsElementPresent(By by)
         {
             try
