3e138687a113937e726c4ebc3e7274a035f63e54
Ermakova Natalia <natalysolitonsf@yandex.com>
2025-09-14 16:31:17 +0300

    upd

--- csharptests/obj/Debug/net9.0/csharptests.AssemblyInfo.cs
+++ csharptests/obj/Debug/net9.0/csharptests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("csharptests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a81f49fd90efecca7320db45b15eeda53500c3a7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc7550940a013accbd7955269125ef11beaa2fd0")]
 [assembly: System.Reflection.AssemblyProductAttribute("csharptests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("csharptests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
